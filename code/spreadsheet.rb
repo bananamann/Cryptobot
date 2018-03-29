d121dfbc6840328889576d3949c01af9df4de523
Pierce <test@test.com>
2018-03-29 01:53:27 -0400

    it now counts orders that were completed during a cycle and outputs that data

--- code/spreadsheet.rb
+++ code/spreadsheet.rb
@@ -5,17 +5,17 @@ require 'net/https'
 require 'json'
 
 
-def get_top_mkts_by_vol
+def get_top_mkts_by_vol(num_results = 5)
   uri = URI.parse "https://www.cryptopia.co.nz/api/GetMarkets/BTC"
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   req = Net::HTTP::Get.new(uri.request_uri)
   response = JSON.parse(http.request(req).body)
 
-  response['Data'].sort_by {|h| h['BaseVolume']}.reverse[0..4] # sort currencies by amount traded in BTC
+  response['Data'].sort_by {|h| h['BaseVolume']}.reverse[0..num_results - 1] # sort currencies by amount traded in BTC
 end
 
-def get_market_orders_by_id(id, num = 100)
+def get_market_orders(id, num = 100)
   uri = URI.parse "https://www.cryptopia.co.nz/api/GetMarketOrders/#{id}/#{num}"
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
@@ -25,8 +25,18 @@ def get_market_orders_by_id(id, num = 100)
   response['Data']
 end
 
-def get_market_state_by_id(id)
+def get_market_state(id)
   uri = URI.parse "https://www.cryptopia.co.nz/api/GetMarket/#{id}"
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  req = Net::HTTP::Get.new(uri.request_uri)
+  response = JSON.parse(http.request(req).body)
+
+  response['Data']
+end
+
+def get_market_history(id)
+  uri = URI.parse "https://www.cryptopia.co.nz/api/GetMarketHistory/#{id}"
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
   req = Net::HTTP::Get.new(uri.request_uri)
@@ -39,9 +49,11 @@ end
 
 # create empty doc up here
 empty_doc = Spreadsheet::Workbook.new
+history_data = []
+
 
 # retrieve currency list
-currencies = get_top_mkts_by_vol
+currencies = get_top_mkts_by_vol 10
 
 # generate empty spreadsheet based on currencies retrieved
 
@@ -53,27 +65,38 @@ empty_doc.write '../output/empty_spreadsheet.xls'
 doc = Spreadsheet.open '../output/empty_spreadsheet.xls'
 
 # __ times, every __ minutes, retrieve and calculate relevant data and put it into the spreadsheet
-for i in 0...1
+for i in 0...3
   # retrieve/calculate all the data from cryptopia
-  # trade_pair_ids.each do |id|
   currencies.each_with_index do |c, j|
     id = currencies[j]['TradePairId']
 
     market_data = currencies[j]
-    order_data = get_market_orders_by_id id
+    order_data = get_market_orders id
+    new_history_data = get_market_history id
+    history_data += [new_history_data]
+
+    total_orders = i == 0 ? 'N/A' : (history_data[j + (currencies.size * i)] - history_data[j + (currencies.size * (i - 1))]).size
 
     buy_orders = order_data['Buy']
     sell_orders = order_data['Sell']
 
+    price = market_data['LastPrice']
+
     buy_sum = buy_orders.inject(0) {|sum, hash| sum + hash['Total']}
     sell_sum = sell_orders.inject(0) {|sum, hash| sum + hash['Total']}
 
+    buy_sell_ratio = (buy_sum / sell_sum).round(5)
+
   # write results to spreadsheet
     sheet = doc.worksheet j
-    sheet.row(0).push 'Price', 'Buy Total', 'Sell Total'
+    sheet.row(0).push 'Price', 'Open Buys Total', 'Open Sells Total', 'Open Order Buy/Sell Ratio', 'Completed Orders' if i == 0
     row = sheet.row(i + 1)
-    row.push market_data['LastPrice'], buy_sum, sell_sum
+    row.push price, buy_sum, sell_sum, buy_sell_ratio, total_orders
   end
 
+  puts "cycle complete"
+
   doc.write '../output/updated_spreadsheet.xls'
+
+  sleep 20
 end
